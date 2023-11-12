7fe5356c12ca70b090d73812c8365e73d1f7146b
danielevardito <danielevito.ardito@studio.unibo.it>
2023-11-12 11:58:48 +0100

    1.1 Definizione di funzioni initPcbs, freePcb, allocPcb

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -5,12 +5,28 @@ LIST_HEAD(pcbFree_h);
 static int next_pid = 1;
 
 void initPcbs() {
+    for(int i = 0; i < MAXPROC; i++){
+        INIT_LIST_HEAD(&pcbTable[i].p_list);
+        list_add_tail(&pcbTable[i].p_list, &pcbFree_h);
+    }
 }
 
 void freePcb(pcb_t *p) {
+    INIT_LIST_HEAD(&p->p_list);
+    list_add_tail(&p->p_list, &pcbFree_h);
 }
 
 pcb_t *allocPcb() {
+    if(list_empty(&pcbFree_h)) return NULL;
+
+    //rimozione del primo PCB in lista
+    pcb_t * p = container_of(&pcbFree_h, pcb_t, p_list);
+    list_del(&p->p_list);
+    //inizializzazione dei campi
+    p->p_pid = next_pid;
+    next_pid++;
+
+    return p;
 }
 
 void mkEmptyProcQ(struct list_head *head) {
