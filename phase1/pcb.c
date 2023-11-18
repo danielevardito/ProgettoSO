bcda9af0cb048cc03c3486768ddf13ddcbce331e
danielevardito <danielevito.ardito@studio.unibo.it>
2023-11-18 10:46:25 +0100

    1.2 Definizione di funzioni in pcb.c

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -22,7 +22,20 @@ pcb_t *allocPcb() {
     //rimozione del primo PCB in lista
     pcb_t * p = container_of(&pcbFree_h, pcb_t, p_list);
     list_del(&p->p_list);
-    //inizializzazione dei campi
+    /*
+    inizializzazione dei campi
+    */
+    /*p_list*/
+    INIT_LIST_HEAD(&p->p_list);
+    /* process tree fields */
+    p->p_parent = NULL;
+    INIT_LIST_HEAD(&p->p_child);
+    INIT_LIST_HEAD(&p->p_sib);
+    /*cpu_t*/
+    p->p_time = 0;
+    /*msg_inbox*/
+    INIT_LIST_HEAD(&p->msg_inbox);
+    //pid
     p->p_pid = next_pid;
     next_pid++;
 
@@ -30,24 +43,55 @@ pcb_t *allocPcb() {
 }
 
 void mkEmptyProcQ(struct list_head *head) {
+    INIT_LIST_HEAD(head);
 }
 
 int emptyProcQ(struct list_head *head) {
+    return list_empty(head);
 }
 
 void insertProcQ(struct list_head *head, pcb_t *p) {
+    list_add_tail(&p->p_list, head);
 }
 
 pcb_t *headProcQ(struct list_head *head) {
+    if(emptyProcQ(head)) return NULL;
+    return container_of(head->next, pcb_t, p_list);
 }
 
 pcb_t *removeProcQ(struct list_head *head) {
+    if(emptyProcQ(head)) return NULL;
+
+    struct list_head * l = head->next;
+    pcb_t *p = container_of(l, pcb_t, p_list);
+    list_del(l);
+
+    return p;
 }
 
 pcb_t *outProcQ(struct list_head *head, pcb_t *p) {
+    struct list_head * l = &p->p_list;
+    int found = 0;
+
+    //itera sulla lista e trova l'elemento in p
+    struct list_head* iter;
+    list_for_each(iter,head) {
+        if(iter == l){
+            found = 1;
+            break;
+        }
+    }
+
+    //se l'elemento non è stato trovato, ritorna NULL
+    if(!found) return NULL;
+
+    //altrimenti rimuovi p dalla lista e ritornalo
+    list_del(l);
+    return p;
 }
 
 int emptyChild(pcb_t *p) {
+    if(list_empty(&p->p_child)) return NULL;
 }
 
 void insertChild(pcb_t *prnt, pcb_t *p) {
